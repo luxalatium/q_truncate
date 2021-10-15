c781109bc97afe9a69c9aa630a360cdbb7ab6533
luxalatium <alu@tacc.utexas.edu>
2021-10-15 17:02:19 -0500

    test change

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 ml gcc
 ml boost
-make clean-all
-make QTRMPI=1 POT_ECKMO=1
-make clean-all
-make QTRMPI=1 POT_ECKHO=1
-make clean-all
-make QTRMPI=1 POT_GAUMO=1
-make clean-all
-make QTRMPI=1 POT_GAUHO=1
-make clean-all
+#make clean-all
+#make QTRMPI=1 POT_ECKMO=1
+#make clean-all
+#make QTRMPI=1 POT_ECKHO=1
+#make clean-all
+#make QTRMPI=1 POT_GAUMO=1
+#make clean-all
+#make QTRMPI=1 POT_GAUHO=1
+#make clean-all
 make QTRMPI=1
 mv qtr_mpi qtr_mpi_hh
